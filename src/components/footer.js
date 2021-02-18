5e07d06698d62ba2d53688e254f37a807a7e39b8
Conrad Trost <ctrost@tamu.edu>
2021-02-18 12:56:34 -0600

    Fixed fontawesome icons large on load

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,9 +17,9 @@ const Footer = () => {
     <footer className={`${
         theme === 'light' ? 'theme-light' : 'theme-dark'} text-main-text bg-transparent absolute bottom-0 text-center w-full`}>
         <div className="m-auto">
-            <a href="https://github.com/retro1967" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub} size="2x" className="transform hover:scale-110 m-5"/></a>
-            <a href="https://linkedin.com/in/conradtrost" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faLinkedin} size="2x" className="transform hover:scale-110 m-5"/></a>
-            <a href="https://twitter.com/TrostDev" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faTwitter} size="2x" className="transform hover:scale-110 m-5" /></a>
+            <a href="https://github.com/retro1967" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub} width="16" size="2x" className="transform hover:scale-110 m-5"/></a>
+            <a href="https://linkedin.com/in/conradtrost" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faLinkedin} width="16" size="2x" className="transform hover:scale-110 m-5"/></a>
+            <a href="https://twitter.com/TrostDev" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faTwitter} width="16" size="2x" className="transform hover:scale-110 m-5" /></a>
         </div>
     </footer>
   )
